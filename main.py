ee967a40a39f4c322e660afa1a27bec703b2f266
Jelmer <jelmervandersaag@me.com>
2023-03-16 12:18:59 +0100

    Fixed main.py to also work on windows

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import subprocess
+import multiprocessing
+import os                                                               
 
-subprocess.run("python3 Asteroids/Asteroids.py & python3 haply_sim/haply.py", shell=True)
+# Creating the tuple of all the processes
+all_processes = ('Asteroids/Asteroids.py', 'haply_sim/haply.py')                                    
+python_path = 'C:/Users/jelme/anaconda3/envs/hri/python.exe' # PUt YOUR CONDA PATH or 'python' for default here
+                                                 
+# This block of code enables us to call the script from command line.                                                                                
+def execute(process):                                                             
+    os.system(f'{python_path} {process}')                                       
 
-
+if __name__ == '__main__':                                                                                                                                                     
+    process_pool = multiprocessing.Pool(processes = 2)                                                        
+    process_pool.map(execute, all_processes)
